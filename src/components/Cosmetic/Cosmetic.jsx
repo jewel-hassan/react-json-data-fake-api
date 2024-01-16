f4816191dbcabaafcd3ce85d03477e2fa903b088
jewel hassan <jewelhassan399@gmail.com>
2024-01-16 16:26:58 +0600

    added new code

--- src/components/Cosmetic/Cosmetic.jsx
+++ src/components/Cosmetic/Cosmetic.jsx
@@ -3,11 +3,16 @@ import "./Cosmetic.css";
 
 const Cosmetic = (props) => {
   const { id, price, product } = props.cosmetics;
+
+  const addToCard =(id)=>{
+    console.log("item is added",id);
+  }
   return (
     <div className="cosmetic">
         <h2>ID:{id}</h2>
       <h3>Bye this:{product}</h3>
       <h4>Only for:{price}</h4>
+      <button onClick={()=> addToCard(id)}>Add to Card</button>
     </div>
   );
 };
